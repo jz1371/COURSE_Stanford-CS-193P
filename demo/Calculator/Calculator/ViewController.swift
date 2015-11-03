baf4be6c100602d542eaa92b975186920174719f
jz1371 <jz1371@nyu.edu>
2015-11-03 16:22:44 -0500

    lecture1 demo

--- demo/Calculator/Calculator/ViewController.swift
+++ demo/Calculator/Calculator/ViewController.swift
@@ -11,10 +11,20 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet weak var display: UILabel!
+    
+    var isUserTypingANumber: Bool = false
 
     @IBAction func appendDigit(sender: UIButton) {
         // declare a constant variable
-        let digit = sender.currentTitle
+        let digit = sender.currentTitle!
+        if isUserTypingANumber {
+            display.text = display.text! + digit
+        } else {
+            display.text = digit
+        }
+        display.text = display.text! + digit
+        println("digit = \(digit)")
+        
     }
 }
 
