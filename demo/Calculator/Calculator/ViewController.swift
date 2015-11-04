f4aeb9dd0355a235f6370b115962a0dd00e9eba0
jz1371 <jz1371@nyu.edu>
2015-11-04 15:06:11 -0500

    lec2: function as argument; closure; positional argument; auto layout

--- demo/Calculator/Calculator/ViewController.swift
+++ demo/Calculator/Calculator/ViewController.swift
@@ -21,10 +21,72 @@ class ViewController: UIViewController {
             display.text = display.text! + digit
         } else {
             display.text = digit
+            isUserTypingANumber = true
         }
-        display.text = display.text! + digit
-        println("digit = \(digit)")
-        
     }
+    
+    @IBAction func operate(sender: UIButton) {
+        let operation = sender.currentTitle!
+        if isUserTypingANumber {
+            enter()
+        }
+        
+        switch operation {
+            // closure1
+            case "×": performOperation({(op1: Double, op2: Double) -> Double in return op1 * op2})
+
+            // neat closure
+            case "÷": performOperation({ (op1, op2) in  op1 / op2})
+
+            // function as argument
+            case "+": performOperation(add)
+            
+            // positional arguments with closure
+            case "−": performOperation { $1 - $0 }
+            
+            // function overloading
+            case "√": performOperation { sqrt($0) }
+            
+            default: break
+        }
+    }
+    
+    private func performOperation(operation: (Double, Double) -> Double) {
+        if (operandStack.count >= 2) {
+            displayValue = operation(operandStack.removeLast(), operandStack.removeLast())
+            enter()
+        }
+    }
+    
+    private func performOperation(operation: Double -> Double) {
+        if (operandStack.count >= 1) {
+            displayValue = operation(operandStack.removeLast())
+            enter()
+        }
+    }
+    
+    func add(op1: Double, op2: Double) -> Double {
+        return op1 + op2
+    }
+    
+    // take advange of type infer instead of var operandStack: Array<Double> = Array<Double>()
+    var operandStack = Array<Double>()
+
+    @IBAction func enter() {
+        isUserTypingANumber = false
+        operandStack.append(displayValue)
+        println("stack: = \(operandStack)")
+    }
+    
+    var displayValue: Double {
+        get {
+            return NSNumberFormatter().numberFromString(display.text!)!.doubleValue
+        }
+        set {
+            display.text = "\(newValue)"
+            isUserTypingANumber = false
+        }
+    }
+
 }
 
