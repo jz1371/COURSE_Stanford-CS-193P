768f0c11769070f9d2674d99e347703b79698379
jz1371 <jz1371@nyu.edu>
2015-11-05 15:52:18 -0500

    Project 1 skeleton

---
+++ assignment/Calculator/Calculator/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Calculator
+//
+//  Created by Jingxin on 11/5/15.
+//  Copyright (c) 2015 NYU. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
